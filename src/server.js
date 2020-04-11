04fe99cb659a792be4133ee7e0888cde76435093
William <william.pinto@lambda3.com.br>
2020-04-11 11:33:22 -0300

    Fixed problem; prepush working

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-const http = require("http")
+const http = require("http");
 const server = http.createServer((request, response) => {
 	response.end(`
         <HTML>
@@ -11,4 +11,4 @@ const server = http.createServer((request, response) => {
         <HTML>`);
 });
 
-server.listen("3000")
+server.listen("3000");
